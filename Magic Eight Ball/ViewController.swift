fbdccd6d2c868fcf4c4adb33a61d6361fbc30044
docmangan <manganmultimedia@gmail.com>
2016-12-13 17:36:50 -0500

    Just added xcode files
    new commit 12/13/16

---
+++ Magic Eight Ball/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Magic Eight Ball
+//
+//  Created by William Thomas Mangan on 12/11/16.
+//  Copyright © 2016 William Mangan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // RANDOM COMMENT
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+         print(error)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
